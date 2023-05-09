1c0e52edbeec9f2792d3efe30e3d1c6fe6ba747a
stvnmc <stvnmc123@gmail.com>
2023-05-09 00:26:57 -0600

    Estados globales asíncronos

--- OB-REACT-REDUX/src/store/config/storeConfig.js
+++ OB-REACT-REDUX/src/store/config/storeConfig.js
@@ -1,8 +1,22 @@
-import { createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension"
+import { compose, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import { rootReducer } from "../reducers/rootReducer";
+import createSagaMiddleware from '@redux-saga/core'
+
 export const createAppStore = () => {
     let store = createStore(rootReducer, composeWithDevTools())
+
+    return store;
+}
+
+export const createAppAsyncStore = () => {
+
+    const sagaMiddleware = createSagaMiddleware();
+
+    let store = createStore(
+        rootReducer,
+        compose(),
+        composeWithDevTools())
 
     return store;
 }
--- OB-REACT-REDUX/src/store/reducers/rootReducer.js
+++ OB-REACT-REDUX/src/store/reducers/rootReducer.js
@@ -1,12 +1,15 @@
 import { combineReducers } from "redux";
 import { todosReducer } from "./todosReducer";
 import { filterReducer } from "./filterReducer";
+import { userReducer } from "./userReducer";
 
 export const rootReducer = combineReducers(
     {
         // state name : redurcer tht will control it 
         todosState: todosReducer,
-        filterState: filterReducer
+        filterState: filterReducer,
+        // ASYNC Example (LOGIN USER)
+        userState: userReducer
         // ... add more states and reducers to include them in the store
     }
 )
