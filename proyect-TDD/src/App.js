c3395953644de64067173378b01e11987245fb9c
stvnmc <stvnmc123@gmail.com>
2023-08-29 20:47:37 -0600

    React testing

--- proyect-TDD/src/App.js
+++ proyect-TDD/src/App.js
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [busqueda, setBusqueda] = useState("");
   return (
     <div className="App">
       <h1>
         welcome to <span>RadioMusic</span>
       </h1>
-      <input type="text" placeholder="write the name of the radio" />
-      <button>Buscar</button>
+      <input
+        type="text"
+        placeholder="write the name of the radio"
+        value={busqueda}
+        onChange={(e) => setBusqueda(e.target.value)}
+      />
+      <button>search</button>
+      <section aria-label="list-stations"></section>
     </div>
   );
 }
--- proyect-TDD/src/App.test.js
+++ proyect-TDD/src/App.test.js
@@ -1,6 +1,6 @@
 // We are going to build an application for the selection and search of streaming radio stations
 
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
 
 // 0 - The app should render correctly
@@ -23,13 +23,60 @@ describe("0 - The application name should be displayed somewhere => RadioMusic",
 });
 
 // 2 - We should be able to search for radios by name,
-// 2a - The application must have an input field with the placeholder => ""
+// 2a - The application must have an input field with the placeholder => "write the name of the radio"
 // 2b - The application must have a search button => text "search"
 // 2c - When we click on the search button, the search function is executed only once
 
-describe("We should be able to search for radios by name", ());
+describe("2 - We should be able to search for radios by name", () => {
+  test('The application must have an input field with the placeholder => "write the name of the radio"', () => {
+    render(<App />);
+    const placeholder = "write the name of the radio";
+    const input = screen.getByPlaceholderText(placeholder);
+    expect(input).toBeInTheDocument();
+  });
+  test('The aplication must have a search button => text "search"', () => {
+    const buttontext = "search";
+    render(<App />);
+    const button = screen.getByText(buttontext);
+    expect(button).toBeInTheDocument();
+  });
+  test("When we click on the search button, the search function is executed only once", () => {
+    render(<App />);
+    const functionMock = jest.fn();
+    const buttontext = "search";
+    const button = screen.getByText(buttontext);
+    button.addEventListener("click", functionMock);
+    fireEvent.click(button);
+  });
+});
 
 // 3 - There must be a list of stations
+// 3a - there must be a list of stations
 // 3a - The list must initialize vacion
 // 3b - When a valid search is made, the list must show at least one result
 // 3c - When we make an invalid search (does not exist), the list should show a message "no stations have been found for this search"
+
+describe("3 - There must be a list of stations", () => {
+  test("there must be a list of stations", () => {
+    render(<App />);
+    const list = screen.getByLabelText("list-stations");
+    expect(list).toBeInTheDocument();
+  });
+  test("The list must initialize emty", () => {
+    render(<App />);
+    const list = screen.getByLabelText("list-stations");
+    const childrenCount = list.childElementCount;
+    expect(childrenCount).toBe(0);
+  });
+  test("When a valid search is made, the list must show at least one result", () => {
+    const placeholder = "write the name of the radio";
+    const input = screen.getByPlaceholderText(placeholder);
+    const buttontext = "search";
+    const button = screen.getByText(buttontext);
+    fireEvent.change(input, { target: { value: "Country" } });
+    fireEvent.click(button);
+    const list = screen.getByLabelText("list-stations");
+    const childrenCount = list.childElementCount;
+    expect(childrenCount).toBeGreaterThanOrEqual(1);
+  });
+});
